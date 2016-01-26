1ea118b85cb9af4b07b2f9fc29ba44a04d3008c9
nadi726 <nadavlevi726@gmail.com>
2016-01-26 20:34:38 +0200

    Removed some more unecessary features and reformatted code

--- file_rename.py
+++ file_rename.py
@@ -5,6 +5,7 @@ import time
 
 directory = raw_input('Enter a directory: ')
 if not directory: directory = os.getcwd()
+print '\n'
 
 def sort_files(order='time'):
     """
@@ -20,30 +21,24 @@ def sort_files(order='time'):
         times = [times.tm_year, times.tm_yday, times.tm_hour, times.tm_min, times.tm_sec]
         lst.append( (times, f) )
 
-    lst.sort()
+    # Sort only be the time value, not the file name
+    lst.sort(key=lambda lst: lst[0])
     return lst
 
-lst = sort_files()
-names_lst = []
-backup = open('backup.txt', 'w')
+names_lst = sort_files()
 
-for t, name in lst:
-    # Writes filenames for the sake of backup and makes a list
-    # Without the now unnecessery time values
-    backup.write( name + '\n' )
-    names_lst.append(name)
-backup.close()
-
-# Stringed numbers for the filenames
-filenames_list = map(str, range(1, len(names_lst)+1))
-
-for count, name in enumerate(names_lst):
+for count, file_tuple in enumerate(names_lst):
+    name = file_tuple[1]
     ext = os.path.splitext(name)[1]
-    join = os.path.join(directory, name)
-    print 'Old: ' + join
+    
+    old_join = os.path.join(directory, name)
+    new_join = os.path.join(directory, str(count)) + ext
+    os.rename(old_join, new_join)
+    
+    print 'Old: ' + old_join
     print
-    new_join = os.path.join(directory, filenames_list[count]) + ext
     print 'New: ' + new_join
-    os.rename(join, new_join)
+    print '-----------------------'
+    
 print 'Done'
 
